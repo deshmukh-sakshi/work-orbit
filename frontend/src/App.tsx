44cf024a60316e700f8514f10bcda0c9a1934411
Sakshi-86 <deshmukhsakshi296@gmail.com>
2025-06-17 12:44:54 +0530

    feat: add tailwindcss setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { TrafficCone } from "lucide-react";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]">
+          <section className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
+            <p className="text-6xl font-extrabold tracking-tight leading-tight sm:text-7xl">
+              Work-Orbit
+            </p>
+            <p className="mt-4 text-lg sm:text-xl text-gray-600">
+              Freelance Job Bidding Platform
+            </p>
 
-export default App
+            <div className="mt-6 flex items-center justify-center gap-3">
+              <p className="text-2xl sm:text-3xl font-medium text-red-500">
+                Work in Progress
+              </p>
+              <TrafficCone size="28" className="text-red-500" />
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
